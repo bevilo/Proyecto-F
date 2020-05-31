c1c72e9cb902a247df82951d3e39ec07c9560d1d
bevilo <65972315+bevilo@users.noreply.github.com>
2020-05-31 04:08:09 +0200

    guardado 6

---
+++ Assets/Scripts/Orbita eliptica.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbitaeliptica : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float velocidad;
+    public Vector3 eje;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation *= Quaternion.AngleAxis(velocidad * Time.deltaTime,eje);
+    }
+}
+
+/*a=1;
+ex=0.017;
+ang=linspace(0,2*pi,100);
+
+d=a*(1-ex^2);
+r=d/(1+ex*cos(ang));
+x=r*cos(ang);
+y=r*sin(ang);*/
+    
